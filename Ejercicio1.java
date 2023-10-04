86fa5bf4fb287adccbaa1d11eeafdaee38409392
Anavpeg552 <anavpeg552@g.educaand.es>
2023-10-04 10:27:44 +0200

    version inicial

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -4,13 +4,13 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
         System.out.println("Introduce tres numeros: ");
-        int a= entrada.nextInt();
-        int b= entrada.nextInt();
+        double a= entrada.nextInt();
+        double b= entrada.nextInt();
         int c= entrada.nextInt();
         entrada.close();
 
         double solucionA=a+b+c*(3-a)/(2*b);
-        int solucionB=(2+(a*b)/4)^c+2;
+        int solucionB=0;
         double solucionC=(-b+a);
         System.out.println("A: "+solucionA+" B: "+ solucionB+ " C: "+ solucionC);
     }
--- Ejercicio2.java
+++ Ejercicio2.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
+        System.out.println("Introduce los grados: ");
+        double G= entrada.nextDouble();
         System.out.println("Introduce F o K: ");
         char T_temp=entrada.next().charAt(0);
         entrada.close();
+
+        double Solucion=(T_temp=='F')?(9*G/5+32):(T_temp=='K')? (G+27315):(0);
+        System.out.println(Solucion+ " "+T_temp);
     }
 }
